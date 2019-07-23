9f0dd9e7e9eb3982e50014b25bb71238912bdbd9
jon.huang <jon.huang@gaiaworks.cn>
2019-07-23 22:41:41 +0800

    解决

--- Hello.java
+++ Hello.java
@@ -17,8 +17,19 @@ public class Hello {
          System.out.println("dd");
     }
 
+<<<<<<< HEAD
 
     justsoso
+=======
+     @Test
+    public  void m() {
+        System.out.println("dd");
+    }
+>>>>>>> remotes/origin/master
     
+
+
+
+    ¼ÓÄÇ¾Í°®
 }
 }
