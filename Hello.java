f92998fa1518d2884c895031ef6192dc73e29b11
jon.huang <jon.huang@gaiaworks.cn>
2019-07-23 22:20:01 +0800

    dassd

--- Hello.java
+++ Hello.java
@@ -17,14 +17,6 @@ public class Hello {
          System.out.println("dd");
     }
 
-     @Test
-    public  void m() {
-        System.out.println("dd");
-    }
-      @Test
-    public  void m() {
-        System.out.println("dd");
-        System.out.println("dd");
-    }
+    
 }
 }
